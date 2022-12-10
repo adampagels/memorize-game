d06dd98962704d84a28df44b3e646f0cc6a70d8d
Adam Pagels <pagels@hotmail.ca>
2022-12-10 10:10:10 +0000

    First commit

---
+++ memorize-game/memorize_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_gameApp.swift
+//  memorize-game
+//
+//  Created by Adam Pagels on 2022-12-09.
+//
+
+import SwiftUI
+
+@main
+struct memorize_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
